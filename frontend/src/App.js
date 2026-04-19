c0738011f9f08beb92e892f8c9be9993263aa3cd
sof_z <sof_z@PC_SZ.(none)>
2026-04-19 16:14:17 -0600

    Proyecto limpio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://18.212.100.215:3001';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
